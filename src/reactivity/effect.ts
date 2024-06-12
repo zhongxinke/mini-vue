a01524cb32e4f5e655bf379ae8e23864aedeeeca
zhongxinke <244729913@qq.com>
2024-06-12 19:29:56 +0800

    feat: 优化stop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,8 @@
 import { extend } from "../share";
 
+let activeEffect;
+let shouldTrack;
+
 class ReactiveEffect {
   private _fn: any;
   deps = [];
@@ -9,8 +12,18 @@ class ReactiveEffect {
     this._fn = fn;
   }
   run() {
+    // 1.会收集依赖
+    // shouldTrack 来做区分
+    if (!this.active) {
+      return this._fn();
+    }
+    shouldTrack = true;
     activeEffect = this;
-    return this._fn();
+
+    const result = this._fn();
+    shouldTrack = true;
+
+    return result;
   }
 
   stop() {
@@ -26,11 +39,14 @@ function cleanupEffect(effect) {
   effect.deps.forEach((dep: any) => {
     dep.delete(effect);
   });
+  effect.deps.length = 0;
 }
 
 const targetMap = new Map();
 
 export function track(target, key) {
+  if (!isTracking()) return;
+
   let depsMap = targetMap.get(target);
 
   if (!depsMap) {
@@ -44,10 +60,14 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
-  if (!activeEffect) return;
+  if (dep.has(activeEffect)) return;
 
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
@@ -62,8 +82,6 @@ export function trigger(target, key) {
     }
   }
 }
-
-let activeEffect;
 
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
