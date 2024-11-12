b60d4fbe417d1815e8eb78e9cb59cf814a440ae4
zhongxinke <244729913@qq.com>
2024-11-12 11:35:01 +0800

    feat: 实现getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -34,9 +34,11 @@ function setupStatefulComponent(instance) {
     const { setup } = Component;
 
     if (setup) {
+        setCurrentInstance(instance)
         const setupResult = setup(shallowReadonly(instance.props), {
             emit: instance.emit
         });
+        setCurrentInstance(null)
         handleSetupResult(instance, setupResult)
     }
 }
@@ -57,4 +59,12 @@ function finishComponentSetup(instance) {
     // if (Component.render) {
     instance.render = Component.render
     // }
+}
+let currentInstance = null;
+export function getCurrentInstance() {
+    return currentInstance
+}
+
+export function setCurrentInstance(instance) {
+    currentInstance = instance
 }
